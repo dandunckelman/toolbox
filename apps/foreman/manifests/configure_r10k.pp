6ada7921e87e8d8764a01a15a8fc43f00293bc4f
Dan Dunckel <dldunckel@gmail.com>
2015-07-02 16:02:15 -0700

    Removed work w/ ssh keys as that was incomplete and work will not continue on this project

--- apps/foreman/manifests/configure_r10k.pp
+++ apps/foreman/manifests/configure_r10k.pp
@@ -1,15 +1,3 @@
-deploy_keys::deploy_key { "r10k-control-repo":
-  ssh_dir     => "/root/.ssh",
-  owner       => "root",
-  group       => "root",
-  private_key => File.read("keys/puppet-repo-private-key")
-  hostname    => "github.com",
-  host_key    => {
-    type     => "ssh-rsa",
-    host_key => File.read("keys/puppet-repo-ssh-key"),
-  }
-}
-
 class { "r10k":
   remote  => "ssh://git@github.com/dandunckelman/puppet.git",
   version => "1.5.1",
