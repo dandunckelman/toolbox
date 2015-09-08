f156ad48b829c5373030edbfebacb48fc5f6853e
Dan Dunckel <dldunckel@gmail.com>
2015-09-08 04:15:35 -0700

    Add validations/scope to Church model

--- apps/google-maps/rails-app/app/models/church.rb
+++ apps/google-maps/rails-app/app/models/church.rb
@@ -1,2 +1,9 @@
 class Church < ActiveRecord::Base
+  validates :name,                  presence: true, uniqueness: true
+  validates :primary_address_1,     presence: true, uniqueness: true
+  validates :primary_address_city,  presence: true
+  validates :primary_address_state, presence: true
+  validates :primary_address_zip,   presence: true
+
+  scope :by_state, ->(state) { where primary_address_state: state }
 end
