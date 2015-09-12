eab0642408e1ee6488dadca74c35a4db958cd01a
Dan Dunckel <dldunckel@gmail.com>
2015-09-11 17:16:49 -0700

    Move back to try going to the get_directions page and changing the JS content to build the map of directions

--- apps/google-maps/rails-app/app/controllers/churches_controller.rb
+++ apps/google-maps/rails-app/app/controllers/churches_controller.rb
@@ -3,7 +3,7 @@ class ChurchesController < ApplicationController
     # TODO
     # - add ability to set queries (and accept user input for certain fields)
     #   - I assume these should map to scopes
-    # - enable pagination
+    # - enable pagination of churches
     @table_headings = [
       'name',
       'rank',
@@ -20,30 +20,26 @@ class ChurchesController < ApplicationController
     # TODO take in the user's current location (or ask for a starting point)
     origin = "8641 Orange Ave. Orange, CA 92865"
 
-    request_data = {
+    @request_data = {
       :avoid        => "tolls",
       :destination  => origin, # origin/destination are the same,
                                # since we want to return to where we started
       :key          => ENV['GOOGLE_MAPS_API_TOKEN'],
-      :mode         => "driving",
+      #:mode         => "driving",
       :origin       => origin,
       :waypoints    => get_waypoints(Church.find(params[:selected_churches_ids]))
     }
-
-    render :json => request_data
   end
 
   private
   # TODO setup params for various queries
 
   def get_waypoints(churches)
-    waypoints = churches.collect do |church|
+    churches.collect do |church|
       "#{church.primary_address_1} " +
         "#{church.primary_address_city}, " +
         "#{church.primary_address_state} " +
         "#{church.primary_address_zip}"
     end
-
-    waypoints.join("|").gsub(" ", "+")
   end
 end
